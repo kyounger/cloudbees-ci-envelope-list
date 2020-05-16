fdf1a7d682c8c3c221741e5638c412fb6855c282
Kenny Younger <kyounger@gmail.com>
2020-05-16 11:06:22 -0500

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+* `cap-plugin-list.sh`: set the envelope and version and it will return formatted list of plugins that are in CAP for that release. Includes an comment that shows how to convert this to a csv.
---
+++ cap-plugin-list.sh
@@ -0,0 +1,6 @@
+VERSION=2.222.2.1
+ENVELOPE=envelope-core-mm
+curl -L "https://jenkins-updates.cloudbees.com/update-center/$ENVELOPE/update-center.json?version=$VERSION" | sed '1d' | sed '$d' > temp.json
+jq '.envelope.plugins | to_entries | [.[] | [.key, .value.name, .value.tier, .value.version]]' temp.json
+# use this if you need to generate a csv:
+# jq -r '.envelope.plugins | to_entries | [.[] | [.key, .value.name, .value.tier, .value.version]][] | @csv' temp.json > cap-plugin-list.csv
